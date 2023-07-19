5cb4261a567436e1fa5539e4d91305a440217fe0
Einsteinia <informationviakate2004@gmail.com>
2023-07-19 18:08:52 +0530

    commit

---
+++ src/Components/Sm.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Sm = () => {
+    return (
+        <>
+            <div className="right_data mt-5" style={{ width: "100%" }}>
+                <div className="Sm mt-5">
+                    <img src="./sign.svg" style={{ maxWidth: 400 }} alt="" />
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Sm
